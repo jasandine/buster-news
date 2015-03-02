abe6a4fb8afd615d8436791d46c8608876f692cb
Joey Sandine and Sean Holstrom <basementbebop@gmail.com>
2015-03-02 12:06:38 -0800

    Create 4 clickable headlines on home page

--- router.js
+++ router.js
@@ -1,5 +1,11 @@
 Buster.Router.map(function() {
   this.resource('posts', {path: '/'});
+  this.resource('hocStory')
+  this.resource('bballStory')
+  this.resource('boehnerStory');
+  this.resource('oilStory');
   this.resource('entertainment');
   this.resource('politics');
+  this.resource('science');
+  this.resource('sports');
 });
